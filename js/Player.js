596fe16a8cba3d05a012b1c5e64a3752980166ea
kqueenli <87553349+kqueenli@users.noreply.github.com>
2023-02-23 16:18:12 -0600

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,59 @@
+class Player {
+  constructor() {
+    this.name = null;
+    this.index = null;
+    this.positionX = 0;
+    this.positionY = 0;
+  }
+
+  // Valor inicial para addPlayer
+   addPlayer() {
+    var playerIndex = "players/player" + this.index;
+
+    if (this.index === 1) {
+      this.positionX = width / 2 - 100;
+    } else {
+      this.positionX = width / 2 + 100;
+    }
+
+    database.ref(playerIndex).set({
+      name: this.name,
+      positionX: this.positionX,
+      positionY: this.positionY,
+    });
+  }
+  //Establecer posicion inicial
+  getDistance() {
+    var playerDistanceRef = database.ref("players/player" + this.index);
+    playerDistanceRef.on("value", data => {
+      var data = data.val();
+      this.positionX = data.positionX;
+      this.positionY = data.positionY;
+    });
+  }
+ 
+  //Bp
+  getCount() {
+    var playerCountRef = database.ref("playerCount");
+    playerCountRef.on("value", data => {
+      playerCount = data.val();
+    });
+  }
+
+  //Bp
+  updateCount(count) {
+    database.ref("/").update({
+      playerCount: count
+    });
+  }
+
+  //AA
+  update() {
+    
+  }
+
+  //Bp
+  static getPlayersInfo() {
+    
+  }
+} 
